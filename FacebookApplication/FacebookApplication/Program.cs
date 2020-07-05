b730226f64b8c38ca4eccb591055b0190cd91003
Inbar Alaluf <inbaralaluf189@gmail.com>
2020-07-05 21:01:46 +0300

    full project

---
+++ FacebookApplication/FacebookApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FacebookApplication
+{
+     // your grade on diagrams document - 100 well done. please see comments inside the document. 50% of your grade).
+     public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormLogin());
+        }
+    }
+}
